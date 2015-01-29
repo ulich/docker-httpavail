09bf3b9883096e7a433508a6d6e03efad2a137df
Timo Ulich <timo.ulich@asideas.de>
2015-01-29 22:38:29 +0100

    Make the expected HTTP status code configurable

--- httpavail.py
+++ httpavail.py
@@ -5,19 +5,23 @@ argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHel
 argparser.add_argument('url')
 argparser.add_argument('-t', '--timeout', type=int, default=120, help='total timeout in seconds to wait for the url to be available')
 argparser.add_argument('-d', '--delay', type=int, default=1, help='delay in seconds between each retry')
+argparser.add_argument('-s', '--status-code', type=int, default=200, help='expected HTTP status code')
 args = argparser.parse_args()
 
 
 @retry(stop_max_delay = args.timeout * 1000, wait_fixed = args.delay * 1000)
 def check():
-    print('Checking', args.url)
+    print('Checking', args.url, 'for HTTP code', args.status_code)
     sys.stdout.flush()
 
     response = requests.get(args.url, timeout = 1)
-    response.raise_for_status()
-    return response.status_code
+    if response.status_code != args.status_code:
+        raise Exception('Unexpected HTTP status code {} (Wanted: {})'.format(response.status_code , args.status_code))
 
-if check() == 200:
+
+try:
+    check()
     sys.exit(0)
-else:
+except Exception as e:
+    print(str(e))
     sys.exit(1)
